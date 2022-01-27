c68c4a7ba778a0d3d17a5b4deb44c6023f140b5f
cdaii <chakibdaii1995@gmail.com>
2022-01-27 20:34:06 +0000

    cdaii - testing recursion examples

--- src/com/example/java/groovy/GroovyJavaSample.groovy
+++ src/com/example/java/groovy/GroovyJavaSample.groovy
@@ -1,5 +1,6 @@
 package com.example.java.groovy
 
+import com.example.java.groovy.recursion.RecursionUtils
 import com.example.java.groovy.sys.calls.GroovySysCalls
 import com.example.java.groovy.sys.calls.JavaSystemCalls
 
@@ -8,5 +9,15 @@ class GroovyJavaSample {
     static void main(String[] args) {
         GroovySysCalls.execute()
         JavaSystemCalls.execute()
+        println RecursionUtils.reverseString("Chakib daii kessou7")
+        println RecursionUtils.isPalindrome("chakibobikahc")
+        println RecursionUtils.findBinary(16653476867645345)
+        println RecursionUtils.recursiveSummation(1403)
+        int[] data = new int[] {1,2,3,4,5,6,7,8,9,10}
+        println RecursionUtils.binarySearch(data,0, data.length-1, 10)
+        println RecursionUtils.fib(10)
+        data = new int[] {-12,2,324,65,98,0,67,33,9,1}
+        RecursionUtils.mergeSort(data, 0, data.length-1)
+        println data
     }
 }

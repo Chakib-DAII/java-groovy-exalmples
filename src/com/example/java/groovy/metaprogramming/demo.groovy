8910023231e03b9b37adc933d8f06766a2a7aaa2
cdaii <chakibdaii1995@gmail.com>
2022-04-22 05:08:58 +0100

    cdaii - closure demo script

---
+++ src/com/example/java/groovy/metaprogramming/demo.groovy
@@ -0,0 +1,13 @@
+c = {
+    append('Jeff')
+    append('Is in Antwerp')
+}
+
+sb = new StringBuffer()
+//delegating a closure to the StringBuffer class to respond to methods inside the closure
+c.delegate = sb
+c()
+
+println "SB: ${sb}"
+    
+    
